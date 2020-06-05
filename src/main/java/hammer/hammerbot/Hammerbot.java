a7810e4a5b142a323f197130c756ff078cdf530d
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-05 06:56:09 -0400

    Initial Commit

---
+++ src/main/java/hammer/hammerbot/Hammerbot.java
@@ -0,0 +1,10 @@
+package hammer.hammerbot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Hammerbot implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
