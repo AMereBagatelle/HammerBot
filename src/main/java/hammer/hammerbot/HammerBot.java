2b12fe1a811c81d35c92dff47aa6a086a80fbd44
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-20 07:16:04 -0400

    Scoreboards is now a Util class

--- src/main/java/hammer/hammerbot/HammerBot.java
+++ src/main/java/hammer/hammerbot/HammerBot.java
@@ -4,11 +4,8 @@ import hammer.hammerbot.settings.SettingsManager;
 import net.fabricmc.api.ModInitializer;
 
 public class HammerBot implements ModInitializer {
-    public static Scoreboards scoreboard;
-
     @Override
     public void onInitialize() {
         SettingsManager.INSTANCE.initSettings();
-        scoreboard = new Scoreboards();
     }
 }
--- src/main/java/hammer/hammerbot/util/Scoreboards.java
+++ src/main/java/hammer/hammerbot/util/Scoreboards.java
@@ -1,4 +1,4 @@
-package hammer.hammerbot;
+package hammer.hammerbot.util;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.scoreboard.ScoreboardObjective;
@@ -8,7 +8,7 @@ import net.minecraft.text.LiteralText;
 import java.util.ArrayList;
 
 public class Scoreboards {
-    public void setScoreboardByName(String name) {
+    public static void setScoreboardByName(String name) {
         MinecraftServer server = (MinecraftServer)FabricLoader.getInstance().getGameInstance();
         ScoreboardObjective objective = server.getScoreboard().getNullableObjective(name);
         if(objective != null) server.getScoreboard().setObjectiveSlot(1, objective); else {
